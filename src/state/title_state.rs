210f6647d96acededfb10013e210ba2379f3a7f2
Bryan McClain <bamcclain10@gmail.com>
2023-05-15 21:18:59 -0400

    Added logic to skip the title screen

--- src/state/title_state.rs
+++ src/state/title_state.rs
@@ -1,8 +1,8 @@
 use std::io::{self, Write};
-use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
-use crossterm::{cursor, QueueableCommand};
+use crossterm::event::{Event, KeyCode, KeyEventKind, KeyModifiers};
+use crossterm::{cursor, event, QueueableCommand};
 
 use super::{LevelSelectState, State};
 use crate::global_state::GlobalState;
@@ -21,14 +21,16 @@ static TITLE: &str = r#"
    ██║   ╚██████╔╝╚██████╔╝███████╗   ██║   ██║  ██║███████╗██║  ██║
    ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
 
-                Created by Bryan McClain
-                 © Comprosoft 2023"#;
+                    Created by Bryan McClain
+                       © Comprosoft 2023"#;
 
-pub struct TitleState;
+pub struct TitleState {
+  now: Instant,
+}
 
 impl TitleState {
   pub fn new() -> Self {
-    Self
+    Self { now: Instant::now() }
   }
 }
 
@@ -48,7 +50,43 @@ impl State for TitleState {
   }
 
   fn execute(self: Box<Self>, _global_state: &mut GlobalState) -> io::Result<Option<Box<dyn State>>> {
-    thread::sleep(Duration::from_secs(1));
-    Ok(Some(Box::new(LevelSelectState::new(0))))
+    let elapsed = self.now.elapsed();
+    if elapsed > Duration::from_secs(2) {
+      return Ok(Some(Box::new(LevelSelectState::new(0))));
+    }
+
+    if event::poll(Duration::from_millis(100))? {
+      // `read()` blocks until an `Event` is available
+      let event = match event::read() {
+        Ok(e) => e,
+        Err(_) => return Ok(None),
+      };
+
+      match event {
+        Event::Resize(_, _) => {
+          return Ok(Some(self));
+        },
+
+        Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
+          KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+            return Ok(None);
+          },
+
+          KeyCode::Enter => return Ok(Some(Box::new(LevelSelectState::new(0)))),
+          KeyCode::Esc => return Ok(None),
+
+          _ => {},
+        },
+
+        _ => {},
+      }
+
+      // Then clear all pending events
+      while event::poll(Duration::from_secs(0))? {
+        event::read()?;
+      }
+    }
+
+    Ok(Some(self))
   }
 }
